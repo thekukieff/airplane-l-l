6f672bcbfe5668dde260020c9fbbd48b95c1ed72
Top <Top@8-2>
2024-10-09 18:27:36 +0500

    Добавьте файлы проекта.

---
+++ airplane l l/Animal.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+
+class Animal {
+public:
+
+
+	void Reproduction() {
+
+	}
+	void Rise() {//рост как процесс
+
+	}
+	void eat() {
+
+	}
+
+private:
+	bool gender_;
+	int age_;
+	int lenght_, width_;
+	int iq_;
+	float massa_;
+};
+
+class Cat: public Animal {
+
+	void Dushat() {
+
+	}
+
+	void Voice(){
+	
+	}
+	void Playing() {
+
+	}
+
+	int size_xvost_;
+	std::string poroda_;
+	std::string name_;
+	std::string color_;
+
+};
+
+class Dog: public Animal {
+	void Dushat() {
+
+	}
+
+	void Voice() {
+
+	}
+	void Playing() {
+
+	}
+	void Walking() {
+
+	}
+
+	int size_xvost_;
+	std::string poroda_;
+	std::string name_;
+
+};
+
+int main() {
+
+
+
+	return 0;
+}
+
+
+
+
+
+
